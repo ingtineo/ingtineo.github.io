6b0425a2b493b2b74f1b18dcfc4b360cc6b5ccd1
ingtineo <156382735+ingtineo@users.noreply.github.com>
2024-11-08 16:53:20 -0400

    Add files via upload

---
+++ mode.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    //PARA CERRAR CON CLICK
+    document.getElementById("closeAlertBtn").addEventListener("click", function () {
+        document.getElementById("darkModeAlert").style.display = "none";
+    });
+});
